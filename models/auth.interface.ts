9ade8f52fdd891935800fc094ccbcff13bdde026
Sarfaraz Ansari <sarfarazdesigner@gmail.com>
2020-06-22 18:16:32 +0530

    updated data model as required

--- models/auth.interface.ts
+++ models/auth.interface.ts
@@ -1,40 +1,45 @@
 export enum UserTypes {
-  SUPER_ADMIN,
-  ADMIN,
-  USER
+  SUPER_ADMIN = 'SUPER_ADMIN',
+  ADMIN = 'ADMIN',
+  USER = 'USER'
 }
 
 export enum GenderType {
-  MALE,
-  FEMALE,
-  TRANSGENDER
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+  TRANSGENDER = 'TRANSGENDER'
 }
 
 export interface IPhoneDetails {
   countryCode: string;
+  dialCode?: string;
+  id?: string;
+  internationalNumber?: string;
+  nationalNumber?: string;
   number: string;
 }
 
 export interface ICommonDetails {
   fullName: string;
   phoneNumber: IPhoneDetails;
-  dob: Date;
+  dob: string;
   gender: GenderType;
 }
 
 export interface IRegisterUser extends ICommonDetails {
   userType: UserTypes;
-  profileImagePath: URL;
+  picture: string;
   email: string; // only valid and unique email IDs
   password: string;
-  address: string;
+  address?: string;
+  city: string;
   country: string;
   emergencyPhoneNumber: IPhoneDetails;
-  isHavingPets: boolean;
-  petsDetail?: string[];
+  isHavingPets?: boolean;
+  petsDetails?: string[];
   referral?: string;
-  isAcceptTermsAndConditions?: boolean;
   familyMembers?: ICommonDetails[];
+  managingFamilies?: string[];
 }
 
 
@@ -43,31 +48,11 @@ export interface ISigninRequest {
   password: string;
 }
 
-export interface IChangePasswordRequest {
-  email: string;
-  oldPassword: string;
-  newPassword: string;
-}
-
-export interface IForgotPasswordRequest {
-  email: string;
-}
-
 export interface ISigninResponse extends IRegisterUser {
-  // jwt token
-  token: string;
-  // randomly generated unique id
-  uuid: string;
-  // user created date
-  createdAt: Date;
-  // last updated
-  updatedAt: Date;
-  // will send an email to confirm the email
-  isEmailVerified: boolean;
-  // if this is an user it will be assigned to admin or super admin
-  // will reference the email here of the manager
-  manager?: string;
-  // if this is an admin
-  isDisabled?: boolean;
+  isActive: boolean;
+  createdAt: number;
+  updatedAt: number;
+  _id: string;
+  guid: string;
   // will add few more data here
 }
